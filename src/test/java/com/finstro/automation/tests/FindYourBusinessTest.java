d68d2ebda7b28ac6890415fb038d57957df26dcf
Tu Nguyen Ngoc <tunnse02859@gmail.com>
2020-01-14 22:50:09 +0700

    update FindBusinessTest

--- src/test/java/com/finstro/automation/tests/FindYourBusinessTest.java
+++ src/test/java/com/finstro/automation/tests/FindYourBusinessTest.java
@@ -1,5 +1,6 @@
 package com.finstro.automation.tests;
 
+import static com.finstro.automation.utility.Assertion.assertTrue;
 import static org.testng.Assert.assertTrue;
 
 import java.lang.reflect.Method;
@@ -11,7 +12,7 @@ import com.finstro.automation.pages.BusinessDetailPage;
 import com.finstro.automation.pages.FindYourBusinessPage;
 import com.finstro.automation.pages.LoginPage;
 import com.finstro.automation.pages.RegisterPage;
-import com.finstro.automation.pages.BusinessCardPage;
+import com.finstro.automation.pages.SelectBusinessCardPage;
 import com.finstro.automation.setup.MobileTestSetup;
 
 public class FindYourBusinessTest extends MobileTestSetup {
@@ -19,7 +20,7 @@ public class FindYourBusinessTest extends MobileTestSetup {
 	private RegisterPage registerPage;
 	private BusinessDetailPage businessDetailPage;
 	private FindYourBusinessPage findYourBusinessScreenPage;
-	private BusinessCardPage businessCardPage;
+	private SelectBusinessCardPage businessCardPage;
 
 	private static final String LOGIN_EMAIL_ADDRESS = "erick@finstro.com.au";
 	private static final String LOGIN_ACCESS_CODE = "033933";
@@ -28,18 +29,24 @@ public class FindYourBusinessTest extends MobileTestSetup {
 	public void setupPage(Method method) throws Exception {
 		registerPage = new RegisterPage(driver);
 		loginPage = new LoginPage(driver);
-		businessCardPage = new BusinessCardPage(driver);
+		businessCardPage = new SelectBusinessCardPage(driver);
 		businessDetailPage = new BusinessDetailPage(driver);
 		findYourBusinessScreenPage = new FindYourBusinessPage(driver);
 		assertTrue(registerPage.isActive(), "Register page didnt showed as default page in first installation");
+		
+		toBusinessDetailPage();
+	}
+	
+	public void toBusinessDetailPage() throws Exception {
+		loginPage.doSuccessLogin(LOGIN_EMAIL_ADDRESS, LOGIN_ACCESS_CODE);
+		businessCardPage.clickOnCard("500");
+		assertTrue(businessDetailPage.isActive(), "Business Details is not  displayed after click on card 500",
+				"Business Details is displayed after click on card 500");
 	}
 
 	@Test
 	public void FPC_1321_VerifyUserRedirectToTheFindYourBusinessScreenSuccessful() throws Exception {
-		loginPage.doSuccessLogin(LOGIN_EMAIL_ADDRESS, LOGIN_ACCESS_CODE);
-		businessCardPage.ClickOnCard("500");
-		assertTrue(businessDetailPage.isActive(), "Business Detail Page is not showed After Select A Card");
 		businessDetailPage.redirectToTheFindYourBusinessScrees();
-		assertTrue(findYourBusinessScreenPage.isActive(), "Find Your Business Page is not showed");
+		assertTrue(findYourBusinessScreenPage.isActive(), "Find Your Business Page is not showed", "Find Your Business Page is showed");
 	}
 }
