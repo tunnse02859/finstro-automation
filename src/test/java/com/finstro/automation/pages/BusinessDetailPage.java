5839ccd20aeae39d0fec20d685e6e4ef0b5a7efa
tunnse02859 <tunnse02859@gmail.com>
2020-01-14 22:38:56 +0700

    resolve conflict

--- src/test/java/com/finstro/automation/pages/BusinessDetailPage.java
+++ src/test/java/com/finstro/automation/pages/BusinessDetailPage.java
@@ -10,7 +10,8 @@ public class BusinessDetailPage {
 
 	private AppiumBaseDriver driver;
 
-	@AndroidFindBy(id = "au.com.finstro.finstropay:id/business_detail_title")
+	@AndroidFindBy(id="au.com.finstro.finstropay:id/business_detail_title")
+	@iOSXCUITFindBy(iOSNsPredicate="name = 'Business Details'")
 	private WebElement title;
 
 	@AndroidFindBy(id = "au.com.finstro.finstropay:id/btnFindBusiness_text")
@@ -21,13 +22,9 @@ public class BusinessDetailPage {
 		PageFactory.initElements(new AppiumFieldDecorator(driver.getDriver()), this);
 	}
 
-	public String GetTitle() {
-		return title.getText();
-	}
-
-	public boolean isActive() throws Exception {
-		return driver.waitForElementDisplayed(title, 10);
-	}
+	public boolean isActive() throws Exception{
+        	return driver.isElementDisplayed(title);
+    	}
 
 	public void redirectToTheFindYourBusinessScrees() throws Exception {
 		driver.click(findYourBusinessButton);
