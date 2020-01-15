23ef0c16e09f85d1fc23af9572fc55ffe6969755
Tu Nguyen Ngoc <tunnse02859@gmail.com>
2020-01-15 19:51:20 +0700

    add Constant variables

---
+++ src/test/java/com/finstro/automation/setup/Constant.java
@@ -0,0 +1,6 @@
+package com.finstro.automation.setup;
+
+public class Constant {
+	public static final String LOGIN_EMAIL_ADDRESS = "erick@finstro.com.au";
+	public static final String LOGIN_ACCESS_CODE = "033933";
+}
