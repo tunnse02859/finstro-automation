a72d608cecfadf80b649d46d70a00bb626cfff80
Tu Nguyen Ngoc <tunnse02859@gmail.com>
2020-03-28 14:31:49 +0700

    test

--- src/test/java/com/finstro/automation/pages/cards/CardsPage.java
+++ src/test/java/com/finstro/automation/pages/cards/CardsPage.java
@@ -3,7 +3,6 @@ package com.finstro.automation.pages.cards;
 import com.finstro.automation.appium.driver.AppiumBaseDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
-import io.appium.java_client.pagefactory.iOSXCUITFindBy;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
